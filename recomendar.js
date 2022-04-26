ef4e515ce0951dc2cf526646b809f24a1cf7c863
Vitor Guimarães <vitorguimaraes.ce@gmail.com>
2022-04-26 09:53:33 -0300

    count liked skills

--- recomendar.js
+++ recomendar.js
@@ -1,18 +1,15 @@
-function recomendar() {
-    
-    var skills = 0;
-    for(var i=0; i<buttons.length; i++) { 
+// Armazena os botões de Recomendar competência
+var buttons = document.getElementsByClassName("artdeco-button artdeco-button--muted artdeco-button--2 artdeco-button--secondary ember-view")  
+var skills = 0; // contador de competências recomendadas
 
-        // Verifica se o texto do botão é igual a "Recomendar competência" ou "Endorse" (versão inglês)
-        if (buttons[i].textContent.includes("Recomendar competência") ||
-            buttons[i].textContent.includes("Endorse")) {
-            // Clica no botão
-            buttons[i].click()
-            skills += 1
-        }   
-    }
-    console.log(skills + " competências recomendadas!")
+for(var i=0; i<buttons.length; i++) { 
+
+    // Verifica se o texto do botão é igual a "Recomendar competência" ou "Endorse" (versão inglês)
+    if ((buttons[i].textContent.includes("Recomendar competência") || buttons[i].textContent.includes("Endorse")) && 
+        !(buttons[i].offsetParent == null) && buttons[i].offsetParent.id.includes("profilePagedListComponent")) {
+        // Clica no botão
+        buttons[i].click()
+        skills += 1
+    }   
 }
-
-var buttons = document.getElementsByClassName("artdeco-button");
-recomendar()
+console.log(skills + " competências recomendadas!")
